358c14ea2b97f9401b0995a87bf8630420603bce
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-10-14 22:52:44 -0500

    Changed permissions

--- core/handlers.rs
+++ core/handlers.rs
@@ -2,49 +2,6 @@
 //!
 //! This module is intended to contain and config within `ServiceRequest` all functions to handle the requests
 //!
-//! # Examples:
-//! ```rust
-//!
-//! use actix_web::HttpResponse;
-//! use actix_web::web::Form;
-//! use diesel::RunQueryDsl;
-//! use core::pool;
-//!
-//! fn handle_info_post(query: Form<models::Info>) -> HttpResponse {
-//!     let query = query.into_inner();
-//!     let models::Info { name } = &query;
-//!
-//!
-//!     use schema::names::dsl as n;
-//!
-//!     diesel::insert_into(n::names)
-//!         .values( &query )
-//!         .execute(&pool::connect())
-//!         .unwrap();
-//!
-//!     HttpResponse::Ok()
-//!         .body(format!("Welcome, {}!\nYou were added to the database", name))
-//! }
-//! ```
-//!
-//! ```rust
-//! use actix_web::HttpResponse;
-//! use core::pool;
-//!
-//! fn handle_info_get() -> HttpResponse {
-//!     use schema::names::dsl as n;
-//!
-//!     let users = n::names
-//!         .filter(n::name_id.gt(2))
-//!         .select((n::name, n::name_id))
-//!         .load::<(String, i64)>(&pool::connect())
-//!         .unwrap();
-//!
-//!
-//!     HttpResponse::Ok()
-//!         .body(format!("Our users are: {:?}", users))
-//! }
-//! ```
 //!
 
 use actix_web::web::{Form};
